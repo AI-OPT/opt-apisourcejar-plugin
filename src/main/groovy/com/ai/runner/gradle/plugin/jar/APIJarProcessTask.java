688c4d38eeb79463e3c252d479e7b8d5d4048ee5
zhangchao2 <zhangchao2@asiainfo.com>
2016-05-01 19:04:50 +0800

    add codes

---
+++ src/main/groovy/com/ai/runner/gradle/plugin/jar/APIJarProcessTask.java
@@ -0,0 +1,67 @@
+package com.ai.runner.gradle.plugin.jar;
+
+import java.io.File;
+import java.util.List;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public class APIJarProcessTask extends DefaultTask {
+
+    /**
+     * 需要解压的jar路径
+     */
+    private List<File> jarPaths;
+
+    /**
+     * 解压后的目标路径
+     */
+    private File destinationDir;
+
+    @Input
+    public File getDestinationDir() {
+        return destinationDir;
+    }
+
+    @Input
+    public List<File> getJarPaths() {
+        return jarPaths;
+    }
+
+    public void setJarPaths(List<File> jarPaths) {
+        this.jarPaths = jarPaths;
+    }
+
+    public void setDestinationDir(File destinationDir) {
+        this.destinationDir = destinationDir;
+    }
+
+    @TaskAction
+    public void unJar() throws Exception {
+        System.out.println("begin to decompress all jars,size="+jarPaths.size());
+        validateParam();
+        if(!destinationDir.exists()){
+            destinationDir = new File(destinationDir.getAbsolutePath());
+            System.out.println("destination dir is not exists,make it!");
+        }
+        System.out.println("destination dir is:"+destinationDir.getAbsolutePath());
+        for (File jarFile : jarPaths) {
+            JarUtil.unJar(jarFile, destinationDir);
+            System.out.println("decompressed successful for jar:"+jarFile.getAbsolutePath());
+        }
+        System.out.println("decompressed all jars successful");
+    }
+
+    private void validateParam() {
+        if (jarPaths == null) {
+            throw new IllegalArgumentException("jarPaths can not be empty");
+        }
+        if (destinationDir == null)
+            throw new IllegalArgumentException("destinationDir can not be empty");
+        if(destinationDir.isFile()){
+            throw new IllegalArgumentException("destinationDir is a file but not dir");
+        }
+    }
+
+}
