18a2cd4fe0ad38e12d99ff43c1ee6f7154dd9e26
gucl <gucl@asiainfo.com>
2016-11-22 13:05:56 +0800

    只解压sourcejar

--- src/main/groovy/com/ai/runner/gradle/plugin/jar/APIJarProcessTask.java
+++ src/main/groovy/com/ai/runner/gradle/plugin/jar/APIJarProcessTask.java
@@ -47,8 +47,12 @@ public class APIJarProcessTask extends DefaultTask {
         }
         System.out.println("destination dir is:"+destinationDir.getAbsolutePath());
         for (File jarFile : jarPaths) {
-            JarUtil.unJar(jarFile, destinationDir);
-            System.out.println("decompressed successful for jar:"+jarFile.getAbsolutePath());
+        	//gucl 20161122
+        	//只解压sources.jar的jar包，即只解压源码jar
+        	if(jarFile.getAbsolutePath().endsWith("sources.jar")){
+        		JarUtil.unJar(jarFile, destinationDir);
+        		System.out.println("decompressed successful for jar:"+jarFile.getAbsolutePath());
+        	}
         }
         System.out.println("decompressed all jars successful");
     }
